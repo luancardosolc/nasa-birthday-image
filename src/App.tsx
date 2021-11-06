d11c7773e2b3d965a8da5799b49c78c65a2ffb6d
Luan Cardoso <luanleitecardoso@gmail.com>
2021-11-06 10:44:24 -0300

    Get input value when the form is submitted

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './giphy.gif';
 import './App.css';
 
 function App() {
+  const [inputs, setInputs] = useState({birthday: ''});
+
+  const handleSubmit = (event: { preventDefault: () => void; }) => {
+    if (event) {
+      event.preventDefault();
+    }
+    console.log('Submit:', inputs);
+  }
+
+  const handleInputChange = (event: { persist: () => void; target: { name: any; value: any; }; }) => {
+    event.persist();
+    setInputs(inputs => ({...inputs, [event.target.name]: event.target.value}));
+  }
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -10,8 +24,8 @@ function App() {
         <p>
           When is your birthday?
         </p>
-        <form>
-          <input type="text" name="birthday" placeholder="Type it here" />
+        <form onSubmit={handleSubmit}>
+          <input type="text" name="birthday" placeholder="YYYY/MM/DD" onChange={handleInputChange} value={inputs.birthday} />
           <br />
           <br />
           <input type="submit" value="Get image of my last Birthday" />
